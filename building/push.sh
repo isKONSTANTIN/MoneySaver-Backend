1d6b5af7a32f705ddfc41048ad9a51b0d27fe109
kostya1661@gmail.com <!#%&(@$^*)lokin135kostya!#%&(@$^*)>
2022-02-21 17:00:59 +0300

    Some build fix

--- building/push.sh
+++ building/push.sh
@@ -2,7 +2,7 @@
 
 echo "Pushing $BRANCH..."
 
-docker build -t ms_backend .
 cat ./pass | docker login https://dockreg.knst.su/ --username ms_push --password-stdin
-docker tag ms_backend dockreg.knst.su/ms_backend.$BRANCH
+docker build -t ms_backend.$BRANCH .
+docker tag ms_backend.$BRANCH dockreg.knst.su/ms_backend.$BRANCH
 docker push dockreg.knst.su/ms_backend.$BRANCH
